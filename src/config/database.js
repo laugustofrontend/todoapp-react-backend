15edaba634beadd87465a6cfbb1aba13ed2c53a8
Lucas Augusto <lucas.augusto5061@gmail.com>
2018-10-27 13:18:41 -0300

    adicionado dotenv no projeto e removido o endereço do banco de dados do código

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,16 @@
 
 const mongoose = require('mongoose')
 
+const infoDb = {
+  host: process.env.MONGO_HOST,
+  pass: process.env.MONGO_PASS,
+  user: process.env.MONGO_USER,
+  port: process.env.MONGO_PORT,
+  db: process.env.MONGO_DB
+}
+
 mongoose.Promise = global.Promise
 
-module.exports = mongoose.connect('mongodb://localhost:27017/todo')
+module.exports = mongoose.connect(`mongodb://${infoDb.user}:${infoDb.pass}@${infoDb.host}:${infoDb.port}/${infoDb.db}`)
+
+// 
